ff5f9da9641d909e8df7450eec5683efb559cc12
eunmi <jiguiiin@naver.com>
2024-09-26 13:08:25 +0900

    엔터 없는 출력

--- src/day01/ExClass01.java
+++ src/day01/ExClass01.java
@@ -9,5 +9,8 @@ public static void main(String[] args) {
 	//sysout 는 괄호의 내용을 출력하는 용도
 	System.out.println(100);
 	System.out.println("안녕하세요");
+	System.out.print("1.안녕");
+	System.out.print("2.안녕");
+	System.out.print("3.안녕");
 }
 }
